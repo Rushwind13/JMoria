2169120d5a102070688c55ee6a1e2b85c530ede2
Michael Schober <mynetlogins.schober@gmail.com>
2024-11-15 20:38:25 -0800

    Mike/feature/remove curse works (#94)
    * feat: Scrolls (#38)
    
    * Happy path remove curse
    
    * Finds the first cursed equipped item and removes curse
    
    HEAD~1
    
    * feat: Scrolls (#38)
    
    * log level debug from info
    
    clang-format
    
    undo updated cursed level
    
    ---------
    
    Co-authored-by: Mike Schober <mike@swiftly.com>

--- src/Player.cpp
+++ src/Player.cpp
@@ -517,15 +517,48 @@ bool CPlayer::Quaff( CLink<CItem> *pLink )
 
 bool CPlayer::Read( CLink<CItem> *pLink )
 {
+    bool retval = true;
     CItem *pItem = pLink->m_lpData;
-    m_llInventory->Remove( pItem->m_pllLink, false );
-    CEffect *pEffect = pItem->m_id->m_llEffects->GetHead()->m_lpData;
-    if( pEffect->m_dwEffect == EFFECT_TYPE_LOSE ) // TODO: add this
+    switch( pLink->m_dwIndex )
     {
-        g_pGame->GetMsgs()->Printf( "It is no longer cursed.\n" );
+    case ITEM_IDX_SCROLL:
+    {
+        JLog( LOG_LEVEL_DEBUG, true, "Reading the %s\n", pItem->GetName() );
+        DoReadScroll( pItem );
+        break;
+    }
+    case ITEM_IDX_BOOK:
+    default:
+    {
+        retval = false;
+    }
     }
 
-    return true;
+    return retval;
+}
+
+void CPlayer::DoReadScroll( CItem *pItem )
+{
+    m_llInventory->Remove( pItem->m_pllLink, false );
+    CEffect *pEffect = pItem->m_id->m_llEffects->GetHead()->m_lpData;
+    // TODO: Fix the effect flag
+    // JLog( LOG_LEVEL_DEBUG, true, "Effect on the item is %d\n", pEffect->m_dwEffect );
+    // if( pEffect->m_dwEffect == EFFECT_TYPE_RESTORE )
+    // {
+    CItem *cursed;
+    CLink<CItem> *pLink = m_llEquipment->GetHead();
+    while( pLink != NULL )
+    {
+        if( pLink->m_lpData->m_dwFlags & ITEM_FLAG_CURSED )
+        {
+            JLog( LOG_LEVEL_DEBUG, true, "Item is cursed %s\n", pLink->m_lpData->GetName() );
+            pLink->m_lpData->m_dwFlags &= ~ITEM_FLAG_CURSED;
+            break;
+        }
+        pLink = pLink->next;
+    }
+    g_pGame->GetMsgs()->Printf( "It is no longer cursed.\n" );
+    // }
 }
 
 bool CPlayer::IsDrinkable( CLink<CItem> *pLink )
